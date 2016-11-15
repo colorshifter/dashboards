fe155de18c0341d049dad95c94061eb1bb3211ab
florianmski <florian.pub@gmail.com>
2016-11-15 15:26:08 +0100

    add missing template to widgets

--- node-server/server/widget/slacker/gallery.js
+++ node-server/server/widget/slacker/gallery.js
@@ -22,6 +22,7 @@ function findImageMessages(dataStore, messages) {
 function createPayload(dataStore, imageMessage) {
   return {
     widgetKey: 'gallery',
+    template: 'image',
     payload: {
       user: dataStore.getUserById(imageMessage.user),
       gallery: {
