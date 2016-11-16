623888dd44d4c6ee950c58a839108cb4817f23d5
Xavi Rigau <xavi@novoda.com>
2016-11-16 13:13:47 +0100

    Replace hax and use environment instead

--- angular2-client/src/app/server-url.service.ts
+++ angular2-client/src/app/server-url.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Config } from './Config';
+import { environment } from './environments/environment';
 
 @Injectable()
 export class ServerUrlService {
 
   mapUrl(): (config: Config) => string {
     return (config: Config) => {
-      let host: string = this.isLocal() ? 'http://localhost' : config.serverUrl;
+      let host: string = environment.production ? config.serverUrl : 'http://localhost';
       return host + ':' + config.port;
     };
-  }
-
-  private isLocal() {
-    return window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
   }
 
 }
