50347a9b57c9e2ffa4475b7ab237344dc6b6fb43
Adam Brown <adam@novoda.com>
2016-11-16 18:18:00 +0000

    adding people-wall skeleton

---
+++ angular2-client/src/app/dashboards/people-wall/people-wall.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { PeopleWallComponent } from './people-wall.component';
+
+describe('Component: PeopleWall', () => {
+  it('should create an instance', () => {
+    let component = new PeopleWallComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular2-client/src/app/dashboards/people-wall/people-wall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-wall',
+  templateUrl: 'people-wall.component.html',
+  styleUrls: ['people-wall.component.scss']
+})
+export class PeopleWallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
