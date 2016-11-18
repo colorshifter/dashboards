3366f427eaeb4970b4ca65096d16677af68ceafc
Adam Brown <adam@novoda.com>
2016-11-18 13:55:59 +0000

    moving opt out list to the server config

--- node-server/server/widget/people-wall.js
+++ node-server/server/widget/people-wall.js
@@ -5,23 +5,20 @@ const valuesOf = require('object.values');
 
 const WAIT_FOR_LOGIN_TIMEOUT_MS = 10 * 1000;
 
-const OPT_OUT_NAMES = [
-  'Alex Styl'
-];
-
 function PeopleWall(config) {
   this.email = config.email;
   this.password = config.password;
+  this.exclude = config.exclude;
 }
 
 PeopleWall.prototype.rank = 1;
 
 PeopleWall.prototype.rule = function () {
-  return findPeople(this.email, this.password)
+  return findPeople(this.email, this.password, excludeFilter(this.exclude))
     .then(toPayload);
 }
 
-function findPeople(email, password) {
+function findPeople(email, password, excludeFilter) {
   return new Promise(function (resolve, reject) {
     horseman
       .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
@@ -32,26 +29,30 @@ function findPeople(email, password) {
       .wait(WAIT_FOR_LOGIN_TIMEOUT_MS)
       .html('#snapshots')
       .close()
-      .then(findPeopleFromSnapshots)
+      .then(findPeopleFromSnapshots(excludeFilter))
       .then(resolve)
       .catch(reject);
   });
 }
 
-function findPeopleFromSnapshots(snapshots) {
-  const $ = cheerio.load(snapshots);
-  const imageElements = $('.camera');
-  return valuesOf(imageElements)
-    .filter((image) => image.attribs && image.attribs.src)
-    .map((image) => {
-      return {
-        image: image.attribs.src,
-        name: image.attribs.title
-      }
-    }).filter(filterOpttedOutPeople);
+function findPeopleFromSnapshots(excludeFilter) {
+  return function(snapshots) {
+    const $ = cheerio.load(snapshots);
+    const imageElements = $('.camera');
+    return valuesOf(imageElements)
+      .filter((image) => image.attribs && image.attribs.src)
+      .map((image) => {
+        return {
+          image: image.attribs.src,
+          name: image.attribs.title
+        }
+      }).filter(excludeFilter);
+  }
 }
 
-const filterOpttedOutPeople = (person) => !OPT_OUT_NAMES.includes(person.name);
+function excludeFilter(exclude) {
+  return (person) => !exclude.includes(person.name);
+}
 
 function toPayload(data) {
   return {
